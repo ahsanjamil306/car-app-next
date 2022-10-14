41b7f3bb00bbef1cd32f7be7230948008f730d40
ahsanjamil306 <ahsanjamil306@gmail.com>
2022-10-14 08:02:32 +0500

    develoed

---
+++ components/SliderData.js
@@ -0,0 +1,18 @@
+export const SliderData = [
+    {
+        image : "https://images.unsplash.com/photo-1494976388531-d1058494cdd8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+        image : "https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+        image : "https://images.unsplash.com/photo-1489824904134-891ab64532f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80",
+    },
+    {
+        image : "https://images.unsplash.com/photo-1511919884226-fd3cad34687c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    },
+    {
+        image : "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=983&q=80",
+    },
+];
+
---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+export const contact = () => {
+  return (
+<div>
+        <Hero heading='My Contact ' message='This is My contact Page' />
+      
+    </div>
+  )
+}
+
+export default contact
